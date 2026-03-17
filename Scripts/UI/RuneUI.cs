edfd91f15ad0ea45112a60b83866363060b2e67b
a595859893 <595859893@qq.com>
2026-03-17 08:29:54 +0800

    REQ-025-01-UI-03: 添加 RuneUI 入口文件

---
+++ Scripts/UI/RuneUI.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using Godot;
+
+/// <summary>
+/// Rune UI - Interface for rune management
+/// </summary>
+public partial class RuneUI : Control
+{
+    // Partial class entries - see Rune/ folder for implementation
+}
