8ee8e352e02812d1a1c3603317b735de793b7394
a595859893 <595859893@qq.com>
2026-03-23 07:06:42 +0800

    impl(REQ-070): TitleCollectionData inherits Resource instead of Object

--- Scripts/Systems/TitleCollection/TitleCollectionData.cs
+++ Scripts/Systems/TitleCollection/TitleCollectionData.cs
@@ -2,7 +2,7 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
-public class TitleCollectionData : Object
+public class TitleCollectionData : Resource
 {
     private Godot.Collections.Dictionary _data = new Godot.Collections.Dictionary();
     
