18a2291994f7f5e075aae9367885847aae3edf39
a595859893 <595859893@qq.com>
2026-03-29 00:16:33 +0800

    chore: sync build artifacts

--- .godot/mono/temp/obj/Debug/ClawRPG.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/ClawRPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClawRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1541e8e0d6510fa694503d0356493ee56efdc323")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6f08546d06478dce2c598cfc0b3e13d9b3fd68")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClawRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClawRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
